0c07915b6af44d791894c1ddd9d329c9d275eb07
HP <HP@BOTNIMOUS>
2022-11-01 22:39:13 +0530

    Add project files.

---
+++ vovelprac/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace vovelprac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a caracter");
+            char ch = Convert.ToChar(Console.ReadLine());
+            if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'I' || ch == 'i' ||
+            ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U')
+            {
+                Console.WriteLine("{0} is a vowel !", ch);
+            }
+            else
+            {
+                Console.WriteLine("{0} IS NOT A VOWEL !", ch);
+            }
+            Console.ReadLine();
+        }
+    }
+}
